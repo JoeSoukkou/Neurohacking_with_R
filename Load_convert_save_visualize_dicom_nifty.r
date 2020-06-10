b6a0fcbc47a2565f59ef168509c7e3b087d37410
Youcef Soukkou <youcefsoko66@gmail.com>
2020-06-10 18:30:20 +0100

    Added an R script for Loading DICOM Files, Converting theÃm to a NIfTI File, Writing/Saving the NIfTI file, Loading the NIfTI Images/Slices & then Visualizing them

---
+++ Load_convert_save_visualize_dicom_nifty.r
@@ -0,0 +1,18 @@
+library("oro.nifti")
+library("oro.dicom")
+# The Output NIfTI file name
+outfname = "OUTPUT_NIfTI_Converted"
+# Changing the working directory
+setwd("C:/Users/Joey S. MD/Desktop/Neurohacking_R/BRAINIX/DICOM")
+# Reading the DICOM T1 Images 
+ALL_T1 = readDICOM('T1/')
+# Converting the Loaded T1 Images to the NIfTI file type using the dicom2nifti function
+NIFTY_Converted = dicom2nifti(ALL_T1)
+# Saving the Converted Slices to a compressed NIfTI File type (nii.gz)
+writeNIfTI(nim=NIFTY_Converted, filename=outfname)
+# Printing relevant metadata of the newley created NIfTI file
+print({nii_T1 = readNIfTI(fname=outfname)})
+#Getting the dim of the NIfTY file
+d = dim(nii_T1)
+# Visualization of the NIfTI Image (Slice 7)
+image(1:d[1], 1:d[2], nii_T1[,,7],xlab="",ylab="")
